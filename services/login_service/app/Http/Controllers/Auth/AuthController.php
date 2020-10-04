c3aa0dc15fcd1fb8f66805b5b830aee24d91b8ec
Nuwan <isharanuwan@gmail.com>
2020-10-04 12:18:07 +0530

    login service init files and sql

---
+++ services/login_service/app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+class AuthController extends Controller
+{
+    Route::post('/sanctum/token', function (Request $request) {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+            'device_name' => 'required',
+        ]);
+    
+        $user = User::where('email', $request->email)->first();
+    
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+    
+        return $user->createToken($request->device_name)->plainTextToken;
+    });
+}
+
+
+
+
