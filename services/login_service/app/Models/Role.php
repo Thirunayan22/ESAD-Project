f32a524b9fa62a13842bdc8a6dd0dcb7fc2e3363
Nuwan <isharanuwan@gmail.com>
2020-10-13 03:49:27 +0530

    role crud added

---
+++ services/login_service/app/Models/Role.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Created by Reliese Model.
+ */
+
+namespace App\Models;
+
+use Carbon\Carbon;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Role
+ * 
+ * @property int $id
+ * @property string $role_name
+ * @property Carbon $created_at
+ * @property Carbon $updated_at
+ * 
+ * @property Collection|User[] $users
+ *
+ * @package App\Models
+ */
+class Role extends Model
+{
+	protected $table = 'roles';
+
+	protected $fillable = [
+		'role_name'
+	];
+
+	public function users()
+	{
+		return $this->hasMany(User::class);
+	}
+}
