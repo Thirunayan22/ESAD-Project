c1cb5bcab4d93202a73c3fae7deb3ac8241c4797
Nuwan <isharanuwan@gmail.com>
2020-10-14 04:53:08 +0530

    enums added

--- services/login_service/config/enum/common.php
+++ services/login_service/config/enum/common.php
@@ -14,6 +14,10 @@ return [
     'active_status' => [
         'ACTIVE' => 1,
         'INACTIVE' => 2
+    ],
+    'verify_status' => [
+        'VERIFIED' => 1,
+        'NOT_VERIFIED' => 0
     ]
 ];
 
