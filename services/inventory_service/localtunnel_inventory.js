c28d2ca00a78a51b76d088542708a1940e3ed71d
Thirunayan <thirunayan22@gmail.com>
2020-10-24 00:09:32 +0530

    inventory_api_tunnel

--- services/inventory_service/localtunnel_inventory.js
+++ services/inventory_service/localtunnel_inventory.js
@@ -1,13 +1,13 @@
 const localtunnel = require('localtunnel');
 
 (async () => {
-  const tunnel = await localtunnel({ port: 5800 , subdomain: "eseller" });
+  const tunnel = await localtunnel({ port: 4100 , subdomain: "eseller" });
 
   // the assigned public url for your tunnel
   // i.e. https://abcdefgjhij.localtunnel.me
   tunnel.url;
   console.log("API running on :" + tunnel.url)
-  console.log("Using port : " + 5800  )
+  console.log("Using port : " + 4100  )
 
   tunnel.on('close', () => {
     //   console.log("Closing tunnel : " , tunnel.url)
