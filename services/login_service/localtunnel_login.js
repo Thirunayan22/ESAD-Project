ed757f9dacd1764273302c916d1d7ec3e5db0264
Thirunayan <thirunayan22@gmail.com>
2020-10-23 23:14:40 +0530

    Updated login tunnel

--- services/login_service/localtunnel_login.js
+++ services/login_service/localtunnel_login.js
@@ -1,13 +1,13 @@
 const localtunnel = require('localtunnel');
 
 (async () => {
-  const tunnel = await localtunnel({ port: 5700 , subdomain: "eseller" });
+  const tunnel = await localtunnel({ port: 4200 , subdomain: "eseller" });
 
   // the assigned public url for your tunnel
   // i.e. https://abcdefgjhij.localtunnel.me
   tunnel.url;
   console.log("API running on :" + tunnel.url)
-  console.log("Using port : " + 5700  )
+  console.log("Using port : " + 4200  )
 
   tunnel.on('close', () => {
     //   console.log("Closing tunnel : " , tunnel.url)
